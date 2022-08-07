af12575f7d77e694fd80139c168758cb6b16e4ca
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2022-08-07 16:48:50 -0400

    Initialized contact app

---
+++ app/src/main/java/com/tochukwuozurumba/contact/AddUpdateContact.java
@@ -0,0 +1,13 @@
+package com.tochukwuozurumba.contact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddUpdateContact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_update_contact);
+    }
+}
