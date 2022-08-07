c4fe07a282a06fc0bbe0127dbb9a186da9cd47e8
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2022-08-07 18:06:15 -0400

    :star: design complete for add and update activity

--- app/src/main/java/com/tochukwuozurumba/contact/AddUpdateContact.java
+++ app/src/main/java/com/tochukwuozurumba/contact/AddUpdateContact.java
@@ -2,12 +2,21 @@ package com.tochukwuozurumba.contact;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class AddUpdateContact extends AppCompatActivity {
+
+//    define contact variables
+    private ImageView profileImageView;
+    private EditText personName, personPhone, personEmail, personNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_update_contact);
+
+//        link variables
+
     }
 }
